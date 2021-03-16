d9ae752f755cd22d5c9fb56b6c2ba90b2c039294
alex pennings <apennings@roc-dev.com>
2021-03-16 14:52:27 +0100

    added hyperspace,added UI

--- Assets/Scripts/Holo_animator.cs
+++ Assets/Scripts/Holo_animator.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Holo_animator : MonoBehaviour
 {
+    public Animator Idle_animator;
+    public Animator SOS_animation;
+    public Animator HyperSpace;
+
+    private bool IsIdle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        IsIdle = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        Debug.Log("trigger enter");
+        if (!IsIdle) { 
+            Debug.Log("is false");
+        if(other.tag == "Player")
+        {
+                Debug.Log("tag is player");
+                IsIdle = true;
+            Idle_animator.enabled = false;
+            SOS_animation.enabled = true;
+        }
+    }
+}
+    public void EnterHyperspace()
+    {
+        HyperSpace.enabled = true;
     }
 }
--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -7,13 +7,19 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     public GameObject PlayerObject;
+    public Camera PlayerCam;
 
     public List<GameObject> Spawnpos;
+    public List<Canvas> canvas;
     // Start is called before the first frame update
     void Start()
     {
         if (PhotonNetwork.IsConnectedAndReady)
         {
+            foreach (Canvas canvas in canvas)
+            {
+                canvas.worldCamera = PlayerCam;
+            }
             PhotonNetwork.Instantiate(PlayerObject.name, Spawnpos[CheckPlayerCount()].transform.position,Quaternion.identity);
         }
     }
