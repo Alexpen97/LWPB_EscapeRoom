9335177bdf24100c3c714c8273a877a74f344ebb
alex pennings <apennings@roc-dev.com>
2021-03-16 23:33:46 +0100

    added lever script

--- Assets/Scripts/Holo_animator.cs
+++ Assets/Scripts/Holo_animator.cs
@@ -6,7 +6,6 @@ public class Holo_animator : MonoBehaviour
 {
     public Animator Idle_animator;
     public Animator SOS_animation;
-    public Animator HyperSpace;
 
     private bool IsIdle;
     // Start is called before the first frame update
@@ -24,19 +23,16 @@ public class Holo_animator : MonoBehaviour
     {
         Debug.Log(other.gameObject.tag);
         Debug.Log("trigger enter");
-        if (!IsIdle) { 
-            Debug.Log("is false");
-        if(other.tag == "Player")
+        if (!IsIdle)
         {
+            Debug.Log("is false");
+            if (other.tag == "Player")
+            {
                 Debug.Log("tag is player");
                 IsIdle = true;
-            Idle_animator.enabled = false;
-            SOS_animation.enabled = true;
+                Idle_animator.enabled = false;
+                SOS_animation.enabled = true;
+            }
         }
-    }
-}
-    public void EnterHyperspace()
-    {
-        HyperSpace.enabled = true;
     }
 }
