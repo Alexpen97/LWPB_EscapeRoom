88a40a1c868e8c52d066d0ac03cc4d192d8e2edc
Bas <s1126560@student.windesheim.nl>
2021-04-28 14:47:55 +0200

    update: box collider fitted, connector un-squashed, lowered animation speed

--- Assets/Powercell/Script/powercell_script.cs
+++ Assets/Powercell/Script/powercell_script.cs
@@ -6,7 +6,7 @@ public class powercell_script : MonoBehaviour
 {
     // charge indicator object and chargespeed variable
     public GameObject chargeObject;
-    public float chargeSpeed = 0.25f;
+    public float chargeSpeed = 0.1f;
 
     public CellState cellState = CellState.FULL;
 
@@ -49,6 +49,10 @@ public class powercell_script : MonoBehaviour
                 chargeh=FULLCHARGE;
                 setScaleY(chargeh);
 
+                // when cell is full, re-enable movement that was disabled by charger for the charging process.
+                var rigi = GetComponent<Rigidbody>();
+                rigi.isKinematic=false;
+
             break;
         }
     }
@@ -80,87 +84,3 @@ public class powercell_script : MonoBehaviour
 public enum CellState {
     EMPTY, CHARGING, FULL
 };
-
-/*
-public class powercell_script : MonoBehaviour
-{
-    // charge indicator object and chargespeed variable
-    public GameObject chargeObject;
-    public float chargeSpeed = 0.25f;
-
-    public CellState cellState = CellState.FULL;
-
-
-    // the current charge value.
-    private float charge = 0f;
-    
-    private const float EMPTYCHARGE=0;//16.2147f;
-    private const float FULLCHARGE=87.2531f;
-
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // set charge to correct base value.
-        updateCharge(cellState, charge);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        switch(cellState){
-            case(CellState.EMPTY):
-                UpdateEmpty();
-            break;
-
-            case(CellState.CHARGING):
-                UpdateCharging();
-            break;
-
-            default:
-            case(CellState.FULL):
-                UpdateFull();
-            break;
-        }
-    }
-
-    void UpdateEmpty(){}
-    void UpdateCharging(){
-        charge+= Time.deltaTime * chargeSpeed;
-        updateCharge(cellState, charge);
-    }
-    void UpdateFull(){}
-
-    
-    void updateCharge(CellState newstate, float newcharge){
-        CellState oldstate=newstate;
-
-        if(newstate==CellState.EMPTY || newcharge <= EMPTYCHARGE ) newcharge=EMPTYCHARGE;
-        if(newstate==CellState.FULL  || newcharge >= FULLCHARGE  ) newcharge=FULLCHARGE;
-        
-        if(cellState!=CellState.CHARGING){
-            chargeObject.GetComponent<Animation>().enabled=false;
-            //chargeObject.GetComponent<Animation>()[].normalizedTime
-        }else{
-            chargeObject.GetComponent<Animation>().Stop();
-            if()
-            chargeObject.GetComponent<Animation>()
-        }
-        
-        chargeObject.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(0, charge));
-       //chargeObject.GetComponent<Animation>().enabled=false;
-
-        cellState=newstate;
-        charge=newcharge;
-
-        if(oldstate!=newstate) Debug.Log("Cell changed state: "+newstate);
-        else Debug.Log("diff:" + (FULLCHARGE-newcharge));
-    }
-}
-
-public enum CellState {
-    EMPTY, CHARGING, FULL
-};
-*/
