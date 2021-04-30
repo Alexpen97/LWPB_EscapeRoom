3b2b55b0f9bc6491c41555925aff801bd4531e4b
DeonicKiller <marco11999@hotmail.com>
2021-04-30 16:44:53 +0200

    Crane colliding with wall

---
+++ Assets/Scissor_lift/Scripts/craneColliderScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class craneColliderScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("je raak me aan");
+    }
+}
