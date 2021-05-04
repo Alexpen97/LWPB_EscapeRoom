5374df13baa72ce992c87ed5200107dfcb89e867
DeonicKiller <marco11999@hotmail.com>
2021-05-04 16:07:15 +0200

    Attach palyer to crane

---
+++ Assets/Scissor_lift/Scripts/platformAttach.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformAttach : MonoBehaviour
+{
+
+    public GameObject vrPlayer;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject == vrPlayer)
+        {
+            vrPlayer.transform.parent = transform;
+            print("ben ik er van af");
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject == vrPlayer)
+        {
+            vrPlayer.transform.parent = null;
+            print("ben ik er van af");
+        }
+    }
+
+
+}
