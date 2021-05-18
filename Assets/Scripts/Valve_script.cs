f4528213a545bd9fe9b750f10911569eead0b184
Alexp <apennings@roc-dev.com>
2021-05-18 10:53:19 +0200

    added a reset to position after valve was grabbed

--- Assets/Scripts/Valve_script.cs
+++ Assets/Scripts/Valve_script.cs
@@ -10,6 +10,7 @@ public class Valve_script : MonoBehaviour
     {
         transform.localRotation = Quaternion.Euler(new Vector3(0, -90, 0));
         transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        transform.localPosition = new Vector3(-0.0372f, 0, 0);
     }
 
 }
