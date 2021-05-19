6d7863fd0db927d61e000883c6cdea73b774aa7e
Bas <s1126560@student.windesheim.nl>
2021-05-19 16:50:09 +0200

    bug fix: relative positioning bug

--- Assets/Generator/internals/GeneratorScript.cs
+++ Assets/Generator/internals/GeneratorScript.cs
@@ -45,35 +45,37 @@ public class GeneratorScript : MonoBehaviour
             var grabi = currentCell.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRGrabInteractable>();
             grabi.interactionLayerMask=0; // make ungrabbable
             grabi.enabled=false;
+            Destroy(grabi,0f);
 
             // make unmovable
             var rigi = currentCell.GetComponent<Rigidbody>();
             rigi.isKinematic=true;
             rigi.useGravity=false;
             rigi.freezeRotation=true;
+            Destroy(rigi,0f);
 
             // set relative to the generator
-            currentCell.transform.parent=this.gameObject.transform;
-            // Snap to position and rotation
-            currentCell.transform.localPosition=new Vector3(0f,0.43f,-0.5f);
-            currentCell.transform.localEulerAngles=new Vector3(-90f,0f,0f);
+            currentCell.transform.parent=gameObject.transform;
 
+            // Snap to position and rotation
+            currentCell.transform.localPosition=new Vector3(0f,0.645f,-0.7f);
+            currentCell.transform.localEulerAngles=new Vector3(-90f,0f,0f);
         }
     }
 
     // Update is called once per frame
-    // if there is a current cell, it should move its relative Z position from -0.5 to 0.3 in about 4 seconds.
+    // if there is a current cell, it should move its relative Z position from -0.7 to 0.5 in about 4 seconds.
     void Update(){
         if(currentCell==null) return;
         
         Vector3 transform=currentCell.transform.localPosition;
 
-        // Z movement: 0.8 meter over moveInTime seconds
-        transform.z += 0.8f / moveInTime * Time.deltaTime;
+        // Z movement: 1.2 meter over moveInTime seconds
+        transform.z += 1.2f / moveInTime * Time.deltaTime;
 
         currentCell.transform.localPosition=transform;
 
-        if(transform.z>=0.3) cellTaken();
+        if(transform.z>=0.5) cellTaken();
     }
 
     // called when the powercell is fully inside the generator.
